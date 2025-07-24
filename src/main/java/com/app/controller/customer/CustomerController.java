eee8ef2059cb859b50e9a107db2350503a134176
yongjinjj <yjnr10@gmail.com>
2025-07-24 11:33:39 +0900

    세션 로그인 관련 처리담당 유틸성 클래스 추가

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.app.common.CommonCode;
 import com.app.dto.user.User;
 import com.app.service.user.UserService;
+import com.app.util.LoginManager;
 
 @Controller
 public class CustomerController {
@@ -66,7 +67,8 @@ public class CustomerController {
 			System.out.println("로그인 성공");
 			
 			//현재 로그인 성공한 사용자 아이디 -> session 영역에 저장
-			session.setAttribute("loginUserId", loginUser.getId());
+			//session.setAttribute("loginUserId", loginUser.getId());
+			LoginManager.setSessionLoginUserId(session, loginUser.getId());
 			
 			//return "redirect:/customer/mypage"; //로그인 성공한 후, 마이페이지로 이동
 			return "redirect:/main";  //로그인 성공한 후, 메인 페이지로
@@ -80,9 +82,11 @@ public class CustomerController {
 		
 		//로그인을 했으면, 로그인 한 사용자의 정보를 보여주는 마이페이지
 		
-		if(session.getAttribute("loginUserId") != null) {  //로그인이 된 상태
+		//if(session.getAttribute("loginUserId") != null) {  //로그인이 된 상태
+		if( LoginManager.isLogin(session) ) {
 
-			String loginUserId = (String)session.getAttribute("loginUserId");
+			//String loginUserId = (String)session.getAttribute("loginUserId");
+			String loginUserId = LoginManager.getLoginUserId(session);
 			
 			//로그인 된 사용자 ID (세션에 저장되어있음)
 			User user = userService.findUserById(loginUserId);
@@ -100,7 +104,8 @@ public class CustomerController {
 	public String logout(HttpSession session) {
 		
 		//세션 초기화
-		session.invalidate();
+		//session.invalidate();
+		LoginManager.logout(session);
 		
 		return "redirect:/main";
 	}
@@ -125,7 +130,8 @@ public class CustomerController {
 		// User 전체 update -> 기존정보는 유지 + 변경할 비밀번호
 		
 		// 특정 id의 비밀번호만 update
-		user.setId((String)session.getAttribute("loginUserId"));
+		//user.setId((String)session.getAttribute("loginUserId"));
+		user.setId( LoginManager.getLoginUserId(session) );
 		
 		System.out.println("비밀번호 변경에 사용할 user 객체");
 		System.out.println(user);
